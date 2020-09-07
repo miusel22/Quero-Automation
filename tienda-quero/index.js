520c109ccda357be1d66d6da82a227c96c8a4a49
iatechdev <Mauricio.roldan@iatech.com.co>
2020-09-07 12:26:07 -0500

    carpetas

---
+++ tienda-quero/index.js
@@ -0,0 +1,127 @@
+function menuresponsive(){
+
+
+    document.getElementById("menures").style.display="block";
+   
+
+}
+
+function displaynone(){
+document.getElementById("menures").style.display="none";
+}
+
+function showwindow(){
+    document.getElementById("vent").style.display="block";
+    
+}
+function noshow(){
+    document.getElementById("vent").style.display="none";
+
+}
+function staffone(){
+    document.getElementById("staffone").style.display="block";
+    document.getElementById("stafftwo").style.display="none";
+    document.getElementById("staffthree").style.display="none";
+   
+}
+function stafftwo(){
+    document.getElementById("staffone").style.display="none";
+    document.getElementById("stafftwo").style.display="block";
+    document.getElementById("staffthree").style.display="none";
+}
+function staffthree(){
+    document.getElementById("staffone").style.display="none";
+    document.getElementById("stafftwo").style.display="none";
+    document.getElementById("staffthree").style.display="block";
+}
+
+function serviceone(){
+    document.getElementById("resone").style.display="block";
+    document.getElementById("restwo").style.display="none";
+}
+function servicetwo(){
+    document.getElementById("resone").style.display="none";
+    document.getElementById("restwo").style.display="block";
+ 
+}
+/*fab*/
+
+function fabone(){
+    document.getElementById("fabone").style.display="block";
+    document.getElementById("fabtwo").style.display="none";
+    document.getElementById("fabthree").style.display="none";
+ 
+}
+function fabtwo(){
+    document.getElementById("fabone").style.display="none";
+    document.getElementById("fabtwo").style.display="block";
+    document.getElementById("fabthree").style.display="none";
+ 
+}
+function fabthree(){
+    document.getElementById("fabone").style.display="none";
+    document.getElementById("fabtwo").style.display="none";
+    document.getElementById("fabthree").style.display="block";
+ 
+}
+function pagone(){
+    document.getElementById("novone").style.display="block";
+    document.getElementById("novtwo").style.display="none";
+    document.getElementById("novthree").style.display="none";
+ 
+}
+function pagtwo(){
+    document.getElementById("novone").style.display="none";
+    document.getElementById("novtwo").style.display="block";
+    document.getElementById("novthree").style.display="none";
+ 
+}
+function pagthree(){
+    document.getElementById("novone").style.display="none";
+    document.getElementById("novtwo").style.display="none";
+    document.getElementById("novthree").style.display="block";
+ 
+}
+
+function documentos(){
+    document.getElementById("down").style.display="block";
+    document.getElementById("cat").style.display="none";
+ 
+}
+function catalogos(){
+    document.getElementById("down").style.display="none";
+    document.getElementById("cat").style.display="block";
+ 
+}
+
+/*fav*/
+function favone(){
+    document.getElementById("favone").style.display="block";
+    document.getElementById("favtwo").style.display="none";
+    document.getElementById("favthree").style.display="none";
+    document.getElementById("favfor").style.display="none";
+ 
+}
+function favtwo(){
+    document.getElementById("favone").style.display="none";
+    document.getElementById("favtwo").style.display="block";
+    document.getElementById("favthree").style.display="none";
+    document.getElementById("favfor").style.display="none";
+ 
+}
+function favthree(){
+    document.getElementById("favone").style.display="none";
+    document.getElementById("favtwo").style.display="none";
+    document.getElementById("favthree").style.display="block";
+    document.getElementById("favfor").style.display="none";
+ 
+}
+function favfor(){
+    document.getElementById("favone").style.display="none";
+    document.getElementById("favtwo").style.display="none";
+    document.getElementById("favthree").style.display="none";
+    document.getElementById("favfor").style.display="block";
+ 
+}
+
+
